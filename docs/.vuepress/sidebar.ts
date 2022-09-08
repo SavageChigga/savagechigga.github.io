c864cda19ec4fb3b1128fe37024145abb19f2fb9
SavageChigga <153486475@qq.com>
2022-09-08 12:33:50 +0800

    11th

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,14 +1,7 @@
 import { sidebar } from "vuepress-theme-hope";
 // .vuepress/config.js
-import { hopeTheme } from "vuepress-theme-hope";
-
-
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
-export default {
-  theme: hopeTheme({
-    iconAssets: "iconfont",
-  }),
-};
+export default 
 sidebar([
   "/DailyRoutine",
   "/Fitness",
